5920dc1fd5751e486b933274a0c5525c250d5fae
iskra17 <rayanami@naver.com>
2023-07-11 14:59:49 +0900

    chore : account 기능 수정 전 pull 받아야함

--- src/routes/Account/Account.tsx
+++ src/routes/Account/Account.tsx
@@ -1,24 +1,31 @@
 import styled from "styled-components";
+import { useUserStore } from "@/store/useUserStore";
+import { Icon } from "@iconify/react";
 
 const Account = () => {
+  const userNickname = useUserStore((state) => state.userNickname);
+  const userId = useUserStore((state) => state.userId);
+
   return (
     <>
       <Title>계정</Title>
       <AccountWrap>
         <ProfileWrap>
           <ProfileImage></ProfileImage>
-          <Username>행복한 사자</Username>
-          <Tag>#1234</Tag>
+          <Username>{userNickname}</Username>
+          <Tag>#{userId?.substring(0, 4)}</Tag>
         </ProfileWrap>
         <InfoWrap>
-        <InfoLeft>
-          <InfoItem>모아요(MOAYO)</InfoItem>
-          <InfoItem>김가은 김경원 김준희 정재현</InfoItem>
-        </InfoLeft>
-        <InfoRight>
-          <InfoItem>깃헙이미지</InfoItem>
-        </InfoRight>
-      </InfoWrap>
+          <InfoLeft>
+            <InfoItem>모아요(MOAYO)</InfoItem>
+            <InfoItem>김가은 김경원 김준희 정재현</InfoItem>
+          </InfoLeft>
+          <InfoRight>
+            <InfoItem>
+              <StyledIcon icon="devicon:github" />
+            </InfoItem>
+          </InfoRight>
+        </InfoWrap>
       </AccountWrap>
     </>
   );
@@ -69,12 +76,12 @@ const ProfileImage = styled.div`
 const Username = styled.p`
   margin-top: 30px;
   margin-bottom: 0;
-  font-weight : 900;
+  font-weight: 900;
 `;
 
 const Tag = styled.p`
   margin-top: 10px;
-  margin-bottom: 0; 
+  margin-bottom: 0;
 `;
 
 const InfoWrap = styled.div`
@@ -83,8 +90,7 @@ const InfoWrap = styled.div`
   width: 100%;
   position: absolute;
   bottom: 0;
-  margin-bottom : 15px;
- 
+  margin-bottom: 15px;
 `;
 
 const InfoLeft = styled.div`
@@ -95,10 +101,15 @@ const InfoLeft = styled.div`
 
 const InfoRight = styled.div`
   text-align: right;
-  margin-right : 15px;
+  margin-right: 15px;
 `;
 
 const InfoItem = styled.p`
- font-weight : 900;
-  margin: 0; 
+  font-weight: 900;
+  margin: 0;
 `;
+
+const StyledIcon =styled(Icon)`
+  width: 38px;
+  height: 38px;
+`
