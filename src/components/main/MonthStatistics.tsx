5ceaae2cf73c0fadd5afeb1d9ca5303474a2e56f
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-12 13:41:22 +0900

    feat : MonthStatistics 지출, 수입 별 통계 및 전체 통계 조건 추가

--- src/components/main/MonthStatistics.tsx
+++ src/components/main/MonthStatistics.tsx
@@ -1,28 +1,54 @@
 import { useState, useEffect } from "react";
 import { Container, Title, DivideContainer, Consumption, Income } from "./MonthStatistics.style";
 import { getPeriodSummary } from "@/api";
+import { search } from "@/types/apiTypes";
+import { useExpensesStore } from "@/store/useExpensesStore";
 
 const MonthStatistics = () => {
   const [title, setTitle] = useState("가계부를 작성해 주세요~");
   const [totalAmount, setTotalAmount] = useState(0);
+  const totalLists = useExpensesStore((state) => state.totalLists);
+  const [consumption, setConsumption] = useState(0);
+  const [income, setIncome] = useState(0);
+
   useEffect(() => {
     getPeriodSummary("monthly", "ozazat").then((res) => {
-      const filteredItem = res.filter((item) => item._id === "2023-07");
+      const filteredItem = res.filter((item: search) => item._id === "2023-07");
       setTotalAmount(filteredItem[0].totalAmount);
     });
-  }, []);
+    totalLists.length === 0
+      ? setTitle("가계부를 작성해주세요~")
+      : totalAmount > 0
+      ? setTitle(`${totalAmount} 원 남았어요!`)
+      : setTitle(`${Math.abs(totalAmount)} 원 사용했네요!`);
+
+    setConsumption(
+      totalLists
+        .filter((item) => item.amount < 0)
+        .reduce((sum: number, item: search) => {
+          return sum + item.amount;
+        }, 0)
+    );
+    setIncome(
+      totalLists
+        .filter((item) => item.amount > 0)
+        .reduce((sum: number, item: search) => {
+          return sum + item.amount;
+        }, 0)
+    );
+  }, [totalLists]);
 
   return (
     <Container>
-      <Title>{totalAmount}</Title>
+      <Title>{title}</Title>
       <DivideContainer>
         <Consumption>
           <p>지출</p>
-          <p>₩ 0</p>
+          <p>₩ {consumption.toLocaleString()}</p>
         </Consumption>
         <Income>
           <p>수입</p>
-          <p>₩ 10000000</p>
+          <p>₩ {income.toLocaleString()}</p>
         </Income>
       </DivideContainer>
     </Container>
