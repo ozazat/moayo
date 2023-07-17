fe42137e43fd8b5d0f9e3860cd8ea94cb9e7b717
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-17 19:15:24 +0900

    style : 주석제거 및 콘솔 제거

--- src/components/main/WeeklyList.tsx
+++ src/components/main/WeeklyList.tsx
@@ -12,44 +12,28 @@ const WeeklyList = () => {
   const [weekList, setWeekList] = useState<{ [key: string]: search[] }>();
   const [isClicked, setIsClicked] = useState<boolean[]>([]);
   const [weekRange, setWeekRange] = useState<{ [key: number]: [number, number] }>({});
-  // const [weekStart, setWeekStart] = useState(0);
-  // const [weekEnd, setWeekEnd] = useState(0);
 
   useEffect(() => {
-    // console.log(dayList);
     createWeekList();
   }, [dayList]);
 
   useEffect(() => {
     const isClickedArray = weekList ? Object.keys(weekList) : [];
     setIsClicked(new Array(isClickedArray.length).fill(false));
-
-    // const aaa = Object.entries(dayList).filter(([day]) => {
-    //   console.log(day.split(".")[1]);
-    //   return Number(day.split(".")[1]) >= 9 && Number(day.split(".")[1]) <= 15;
-    // });
-    // console.log(aaa);
   }, [weekList]);
-
-  // useEffect(() => {
-  //   console.log(isClicked);
-  // }, [isClicked]);
 
   const createWeekList = () => {
     const newWeekList: { [key: string]: search[] } = {};
     Object.entries(dayList).forEach(([day, expenseList]) => {
-      // console.log("expenseList", expenseList);
       const newDay = `${currentYear}.${day}`;
       const formattedNewDay = new Date(newDay);
       const newWeekNumber = getWeekNumber(formattedNewDay);
       const [start, end] = getWeekRange(formattedNewDay, newWeekNumber);
-      // const formatWeekRange =
       if (start.getDate() <= Number(day.split(".")[1]) && end.getDate() >= Number(day.split(".")[1])) {
         if (!newWeekList[`${start.toLocaleDateString()} ~ ${end.toLocaleDateString()}`]) {
           newWeekList[`${start.toLocaleDateString()} ~ ${end.toLocaleDateString()}`] = [];
         }
         newWeekList[`${start.toLocaleDateString()} ~ ${end.toLocaleDateString()}`].push(...expenseList);
-        // console.log("newWeekList", newWeekList);
         setWeekList(newWeekList);
       }
     });
@@ -57,12 +41,9 @@ const WeeklyList = () => {
 
   const ContainerClickHandler = (day: string, index: number) => {
     setIsClicked(isClicked.map((clicked, clickedIndex) => (clickedIndex === index ? !clicked : clicked)));
-    // day = [2023, 07, 05, 2023, 07, 10]
     const dates: number[] = day.match(/\d+/g)?.map(Number) || [];
-    // console.log(dates);
     const start = dates[2];
     const end = dates[5];
-    // console.log(start, end);
     setWeekRange((prevWeekRange) => ({ ...prevWeekRange, [index]: [start, end] }));
   };
 
@@ -97,7 +78,6 @@ const WeeklyList = () => {
                 Object.entries(dayList)
                   .filter(([day]) => {
                     const [weekStart, weekEnd] = weekRange[index];
-                    console.log(weekStart, weekEnd, day);
                     return Number(day.split(".")[1]) >= weekStart && Number(day.split(".")[1]) <= weekEnd;
                   })
                   .map(([day, lists]) => <DailyList key={day} day={day} expenseList={lists} />)}
@@ -116,9 +96,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  // border: 1px solid blue;
-  /* min-width: 330px;
-  min-height: 600px; */
   width: 350px;
   height: 440px;
   max-width: 350px;
