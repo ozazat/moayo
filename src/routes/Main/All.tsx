6d3b50a68c6c1bdf9616d8b1ceb5866c9cf4cadf
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-20 14:17:34 +0900

    refactor : 전체, 일간 데이터 함수 script 로 뺌

--- src/routes/Main/All.tsx
+++ src/routes/Main/All.tsx
@@ -5,43 +5,15 @@ import { search, ObjectData } from "@/types/apiTypes";
 import DailyList from "@/components/main/DailyList";
 import { PostBtn } from "@/components/PostBtn";
 import MonthStatistics from "@/components/main/MonthStatistics";
+import { createList } from "@/scripts/createList";
 
 const All = () => {
   const totalLists = useExpensesStore((state) => state.totalLists);
   const [allLists, setAllLists] = useState<ObjectData>({});
 
   useEffect(() => {
-    createAllList();
+    createList(totalLists, setAllLists);
   }, [totalLists]);
-
-  const createAllList = () => {
-    const newDayList: { [key: string]: search[] } = {};
-    totalLists.forEach((list) => {
-      const date = list.date;
-      const [year, month, dayTime] = date.split("-");
-      const day = dayTime.split("T")[0];
-      const formattedDay = `${year}.${month}.${day}`;
-      if (newDayList[formattedDay]) {
-        newDayList[formattedDay].push(list);
-      } else {
-        newDayList[formattedDay] = [list];
-      }
-    });
-    // console.log("newDayList", newDayList);
-    const keysArray: string[] = Object.keys(newDayList);
-
-    // 키 값을 내림차순으로 정렬합니다.
-    keysArray.sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
-
-    // 정렬된 키를 기반으로 새로운 객체를 생성합니다.
-    const sortedData: ObjectData = {};
-    keysArray.forEach((key) => {
-      sortedData[key] = newDayList[key];
-    });
-
-    console.log("sortedData", sortedData);
-    setAllLists(sortedData);
-  };
 
   return (
     <MainContainer>
