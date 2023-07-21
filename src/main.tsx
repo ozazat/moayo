fc5136f3b5285005710a8401dfb2fec9ce19d7a1
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-21 15:19:50 +0900

    build : 제발 (basename추가)

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
